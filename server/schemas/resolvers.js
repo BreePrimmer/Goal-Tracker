750e6d0f054d702f60011a2e5a764f50c7de1889
PsychoRSS <Bobgonzalez516@gmail.com>
2023-11-15 19:41:05 -0700

    added newUser and login resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,7 +6,35 @@ const resolvers = {
     users: async () => {
       return await User.find({});
     }
+  },
+
+  Mutation: {
+    newUser: async (parent, {username, email, password}) => {
+      const newUser = await User.create({username, email, password});
+      const token = signToken(newUser);
+
+      return {newUser, token};
+    },
+     login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
   }
+
+
 };
 
 module.exports = resolvers;
