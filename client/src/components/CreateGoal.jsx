0b748a8a9a397db68cb7b0561215178490c3c309
Javi <nb2javi@gmail.com>
2023-11-15 19:34:14 -0600

    Added more to creategoal form

--- client/src/components/CreateGoal.jsx
+++ client/src/components/CreateGoal.jsx
@@ -1,9 +1,20 @@
 export default function CreateGoal() {
   return (
     <div>
+      <span>Make a new goal</span>
       <form>
-        <label for="category">Category:</label>
-        <input type="text" name="category" id="" />
+        <div>
+          <label for="newGoalCategory">Category:</label>
+          <input type="text" name="newGoalCategory" id="" />
+        </div>
+        <div>
+          <label for="newGoalDesc">Description:</label>
+          <input type="text" name="newGoalDesc" />
+        </div>
+        <div>
+          <label for="newGoalDate">End Date</label>
+          <input type="datetime-local" />
+        </div>
       </form>
     </div>
   );
