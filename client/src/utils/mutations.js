4d849f27cac0a71909dc4ec4d3afdcd0b8e1e355
PsychoRSS <Bobgonzalez516@gmail.com>
2023-11-16 18:56:27 -0700

    Create mutations.js
    Added base mutations for the front the last 3 will probably be changed because the back end mutations haven't finished just yet

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,62 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+login(email: $email, password: $password) {
+    token
+    user {
+    _id
+    username
+    }
+}
+`;
+
+export const NEW_USER = gql`
+mutation newUser($username: String!, $email: String!, password: String!) {
+    newUser(username: $username, email: $email, psasword: $password) {
+        token
+        user {
+            _id
+            username
+        }
+    }
+}
+`;
+
+export const NEW_CATEGORY = gql`
+mutation newCategory($name: String, user:ID) {
+    newCategory (name: $name, userID: user) {
+        name
+        user {
+            username
+        }
+    }
+}
+`;
+
+export const NEW_TO_DO = gql`
+mutation newToDo($user: ID!, text: String!, completed: Boolean) {
+    newToDo (user: user, text: text, completed: completed) {
+        text
+        user {
+            username
+        }
+    }
+}
+`;
+
+export const DELETE_CATEGORY = gql`
+mutation deleteCategory($CatId: ID!) {
+    deleteCategory(CatID: CatId){
+        _id
+    }
+}
+`;
+
+export const DELETE_TODO = gql` 
+mutation deleteToDo($toDoId: ID!){
+    deleteToDo(toDoId: toDoId) {
+        _id
+    }
+}
+`;
