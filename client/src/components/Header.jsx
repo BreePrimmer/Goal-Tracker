9029c3d0aae978de13d11d2a601859d99aa92bc4
Javi <nb2javi@gmail.com>
2023-11-16 19:43:39 -0600

    App name takes user to home

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
-    <header className='header-container'>
-      <h1 className='title'>Goal Tracker</h1>
+    <header className="header-container">
+      <Link to="/">
+        <h1 className="title">Goal Tracker</h1>
+      </Link>
 
       <nav>
         <ul>
-          <li className='login'>Login</li>
+          <li className="login">Login</li>
         </ul>
       </nav>
     </header>
