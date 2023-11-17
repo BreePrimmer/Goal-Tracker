c0dac29ed93ae0307c912cd515abb7286cb94427
Javi <nb2javi@gmail.com>
2023-11-16 19:19:12 -0600

    Finished creategoal form Javascript

--- client/src/components/CreateGoal.jsx
+++ client/src/components/CreateGoal.jsx
@@ -2,14 +2,25 @@ import { useState } from "react";
 
 export default function CreateGoal() {
   const [longTerm, setLongTerm] = useState(true);
+  const [category, setCategory] = useState("");
+  const [description, setDescription] = useState("");
+  const [endDate, setEndDate] = useState("");
 
   const goalFormHandler = (e) => {
     e.preventDefault();
+    console.log(category);
+    console.log(description);
+    console.log(`Long term: ${longTerm}`);
+    console.log(endDate);
+    setLongTerm(true);
+    setCategory("");
+    setDescription("");
+    setEndDate("");
   };
   return (
     <div className="goal-cont">
       <span id="new-goal">Make a new goal</span>
-      <form>
+      <form onSubmit={goalFormHandler}>
         <div className="goal-spacing">
           <label className="form-title" htmlFor="newGoalCategory">
             Category:
@@ -19,6 +30,10 @@ export default function CreateGoal() {
             type="text"
             name="newGoalCategory"
             id=""
+            value={category}
+            onChange={(e) => {
+              setCategory(e.target.value);
+            }}
           />
         </div>
         <div className="goal-spacing" id="desc-cont">
@@ -30,17 +45,41 @@ export default function CreateGoal() {
             id="desc"
             type="text"
             name="newGoalDesc"
+            value={description}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
           />
         </div>
         <div className="goal-spacing">
-          <span className="form-title">Short term</span> or{" "}
-          <span className="form-title">Long term</span>
+          <span
+            className="form-title"
+            onClick={() => {
+              setLongTerm(false);
+            }}>
+            Short term
+          </span>{" "}
+          or{" "}
+          <span
+            className="form-title"
+            onClick={() => {
+              setLongTerm(true);
+            }}>
+            Long term
+          </span>
         </div>
         <div className="goal-spacing">
           <label className="form-title" htmlFor="newGoalDate">
             End Date
           </label>
-          <input className="form-input" type="datetime-local" />
+          <input
+            className="form-input"
+            type="datetime-local"
+            value={endDate}
+            onChange={(e) => {
+              setEndDate(e.target.value);
+            }}
+          />
         </div>
         <button type="submit">Create Goal</button>
       </form>
