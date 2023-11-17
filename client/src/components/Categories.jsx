df4670e302191bb0373988d0420875baf78e2c0e
Javi <nb2javi@gmail.com>
2023-11-16 19:35:09 -0600

    Form for adding new category done

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,10 +1,42 @@
+import { useState } from "react";
+
 export default function Categories() {
+  const [createCategory, setCreateCategory] = useState(false);
+  const [newCategory, setNewCategory] = useState("");
+
+  const categoryFormHandler = (e) => {
+    e.preventDefault();
+    console.log(newCategory);
+    setNewCategory("");
+    setCreateCategory(false);
+  };
+
   return (
-    <div className='category-container'>
-      <ul className='categories'>
-        <li id='my-category'>My categories</li>
-        <li className='category'>General</li>
-        <li className='category'>New +</li>
+    <div className="category-container">
+      <ul className="categories">
+        <li id="my-category">My categories</li>
+        <li className="category">General</li>
+        <li
+          className="category"
+          onClick={() => {
+            setCreateCategory(true);
+          }}>
+          New +
+        </li>
+        {createCategory && (
+          <form onSubmit={categoryFormHandler}>
+            <input
+              type="text"
+              name="newCategory"
+              id="newCategory"
+              value={newCategory}
+              onChange={(e) => {
+                setNewCategory(e.target.value);
+              }}
+            />
+            <button type="submit">Create</button>
+          </form>
+        )}
       </ul>
     </div>
   );
