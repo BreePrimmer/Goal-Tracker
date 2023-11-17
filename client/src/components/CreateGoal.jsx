efc00540f5af28e69a7938ecafc0661798164c6e
breelovesfrogs <134086243+breelovesfrogs@users.noreply.github.com>
2023-11-16 18:56:53 -0600

    finished css for new goal for now

--- client/src/components/CreateGoal.jsx
+++ client/src/components/CreateGoal.jsx
@@ -1,22 +1,22 @@
 export default function CreateGoal() {
   return (
-    <div>
+    <div className='goal-cont'>
       <span id='new-goal'>Make a new goal</span>
       <form>
         <div className='goal-spacing'>
           <label className='form-title' htmlFor="newGoalCategory">Category:</label>
-          <input type="text" name="newGoalCategory" id="" />
+          <input className='form-input' type="text" name="newGoalCategory" id="" />
         </div>
-        <div className='goal-spacing'>
+        <div className='goal-spacing' id='desc-cont'>
           <label className='form-title' htmlFor="newGoalDesc">Description:</label>
-          <input type="text" name="newGoalDesc" />
+          <textarea className='form-input' id='desc' type="text" name="newGoalDesc" />
         </div>
         <div className='goal-spacing'>
-          <span className='form-title'>Short term</span> or <span>long term</span>
+          <span className='form-title'>Short term</span> or <span className='form-title'>Long term</span>
         </div>
         <div className='goal-spacing'>
           <label className='form-title' htmlFor="newGoalDate">End Date</label>
-          <input type="datetime-local" />
+          <input className='form-input' type="datetime-local" />
         </div>
       </form>
     </div>
