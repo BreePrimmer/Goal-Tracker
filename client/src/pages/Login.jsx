a0abacb126a864b8dcab9bc871d612acb2853cf9
Javi <nb2javi@gmail.com>
2023-11-20 19:38:25 -0600

    Optimized code for login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,34 +2,64 @@ import { useState } from "react";
 
 export default function Login() {
   const [loggingIn, setLoggingIn] = useState(true);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginUser = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    console.log("User logged in!");
+  };
+  const signupUser = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    console.log("User signed up!");
+  };
   return (
     <div>
+      <form onSubmit={loggingIn ? loginUser : signupUser}>
+        <label htmlFor="Username">Username:</label>
+        <input
+          type="text"
+          name="Username"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <label htmlFor="Password">Password:</label>
+        <input
+          type="password"
+          name="Password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button type="submit">{loggingIn ? `Log in` : `Sign up`}</button>
+      </form>
+
       {loggingIn ? (
-        <form>
-          <label htmlFor="Username">Username:</label>
-          <input type="text" name="Username" />
-          <label htmlFor="Password">Password:</label>
-          <input type="password" name="Password" />
-          <button type="submit">Login</button>
-        </form>
-      ) : (
-        <form>
-          <label htmlFor="Username">Username:</label>
-          <input type="text" name="Username" />
-          <label htmlFor="Password">Password:</label>
-          <input type="password" name="Password" />
-          <button type="submit">Login</button>
-        </form>
-      )}
-      <span>
-        New to us?{" "}
-        <span
-          onClick={() => {
-            setLoggingIn(false);
-          }}>
-          Sign up!
+        <span>
+          New to us?{" "}
+          <span
+            onClick={() => {
+              setLoggingIn(false);
+            }}>
+            Sign up!
+          </span>
         </span>
-      </span>
+      ) : (
+        <span>
+          Logging in?{" "}
+          <span
+            onClick={() => {
+              setLoggingIn(true);
+            }}>
+            Sign up!
+          </span>
+        </span>
+      )}
     </div>
   );
 }
