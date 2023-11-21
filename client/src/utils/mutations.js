8cac7cf50869cab42cbab496cd73d5b3a7824b45
cgwol <cgwalterson@icloud.com>
2023-11-20 19:39:42 -0700

    updated client apollo

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,18 +2,7 @@ import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
 mutation login($email: String!, $password: String!) {
-login(email: $email, password: $password) {
-    token
-    user {
-    _id
-    username
-    }
-}
-`;
-
-export const NEW_USER = gql`
-mutation newUser($username: String!, $email: String!, password: String!) {
-    newUser(username: $username, email: $email, psasword: $password) {
+    login(email: $email, password: $password) {
         token
         user {
             _id
@@ -23,9 +12,21 @@ mutation newUser($username: String!, $email: String!, password: String!) {
 }
 `;
 
+export const NEW_USER = gql`
+mutation newUser($username: String!, $email: String!, $password: String!) {
+    newUser(username: $username, email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+        }
+    }
+}  
+`;
+
 export const NEW_CATEGORY = gql`
-mutation newCategory($name: String, user:ID) {
-    newCategory (name: $name, userID: $user) {
+mutation newCategory($name: String, $user: ID) {
+    newCategory(name: $name, user: $user) {
         name
         toDo {
             completed
@@ -36,18 +37,19 @@ mutation newCategory($name: String, user:ID) {
             _id
         }
     }
-}
+}  
 `;
 
 export const NEW_TO_DO = gql`
-mutation newToDo($user: ID!, text: String!, completed: Boolean) {
-    newToDo (user: $user, text: $text, completed: $completed) {
+mutation newToDo($user: ID!, $text: String!, $completed: Boolean) {
+    newToDo(user: $user, text: $text, completed: $completed) {
         _id
         text
         completed
         date
         user {
             username
+            _id
         }
     }
 }
@@ -77,18 +79,17 @@ mutation deleteToDo($toDoId: ID!){
 `;
 
 export const COMPLETED_TODO = gql`
-mutation completedToDo ($user: ID!, $toDoId: ID!) {
-    completeToDo(userId: $user, toDoId: $toDoID) {
+mutation completedToDo($user: ID!, $toDoId: ID!) {
+    completeToDo(user: $user, toDoId: $toDoId) {
         _id
         text
         completed
         date
-        user{
+        user {
             _id
             username
         }
     }
-}
-
+}  
 `
 
