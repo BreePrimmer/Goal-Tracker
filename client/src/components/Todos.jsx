c77c0183fea8ac797159d456dab8e1e7593096c8
Javi <nb2javi@gmail.com>
2023-11-22 19:34:53 -0600

    Merged todos.js

--- client/src/components/Todos.jsx
+++ client/src/components/Todos.jsx
@@ -1,10 +1,6 @@
 import { useState } from "react";
 
-<<<<<<< HEAD
-export default function Todos(props) {
-=======
 export default function Todos() {
->>>>>>> a96b3a1b5e3c8c86f660e9810df738fdc7dc83da
   const [createTodo, setCreateTodo] = useState(false);
   const [newTodo, setNewTodo] = useState("");
 
@@ -16,31 +12,16 @@ export default function Todos() {
     setNewTodo("");
     console.log(newTodo);
   };
-<<<<<<< HEAD
-  // [{"__typename":"Todo","_id":"655d92f6d18bba64a1805044","text":"Do Homework 1.2"}]
-=======
->>>>>>> a96b3a1b5e3c8c86f660e9810df738fdc7dc83da
 
   return (
     <div className="todo-cont">
       <h1 id="to-do">To-do's</h1>
       <ul className="to-do-ul">
-<<<<<<< HEAD
         {props.userData.map((todo) => (
           <li className="to-do-li" key={todo._id}>
             {todo.text}
           </li>
         ))}
-=======
-        <li className="to-do-li">Placeholder todo</li>
-        <li className="to-do-li">Placeholder todo</li>
-        <li className="to-do-li">Placeholder todo</li>
-        {/* {exampleList.map((todo, index) => (
-          <li className="to-do-li" key={todo}>
-            {todo}
-          </li>
-        ))} */}
->>>>>>> a96b3a1b5e3c8c86f660e9810df738fdc7dc83da
       </ul>
 
       {!createTodo ? (
