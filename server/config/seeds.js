a5262385d74a7188bed9f2c6a64948f9c2712960
cgwol <cgwalterson@icloud.com>
2023-11-27 00:21:02 -0700

    fixed seeds

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -8,64 +8,58 @@ db.once('open', async () => {
     try {
         await User.deleteMany();
 
-        await User.create({
+        const user1 = await User.create({
             username: 'user1',
             email: 'user1@email.com',
             password: 'password',
-            categories: [
-                {
-                    name: 'General',
-                    goals: [
-                        { title: 'School report', text: 'Finish report', completed: false, date: new Date(), _id: new ObjectId() },
-                        { title: 'Attend Class' ,text: 'Attend Class', completed: true, date: new Date(), _id: new ObjectId() }
-                    ]
-                }
-            ],
-            todos: [
-                { text: 'Do Homework 1.2' }
-            ]
-        })
+        });
 
-        await User.create({
+        const user2 = await User.create({
             username: 'user2',
             email: 'user2@email.com',
             password: 'password',
-            categories: [
-                {
-                    name: 'General',
-                    goals: [
-                        {title: 'test title', text: 'test', completed: false, date: new Date(), _id: new ObjectId() },
-                    ]
-                },
-                {
-                    name: 'Personal',
-                    goals: [
-                        { title: 'DOG' ,text: 'Walk the dog', completed: false, date: new Date(), _id: new ObjectId() },
-                        { title: 'Dinner Reservations' ,text: 'Eat Dinner', completed: true, date: new Date(), _id: new ObjectId() },
-                        { title: 'Workout', text: 'Goto the gym', completed: false, date: new Date(), _id: new ObjectId() },
-                    ]
-                },
-                {
-                    name: 'Work',
-                    goals: [
-                        { title: 'Work', text: 'Attend Meeting', completed: true, date: new Date(), _id: new ObjectId() },
-                        { title: 'Work', text: 'Send resignation letter', completed: false, date: new Date(), _id: new ObjectId() },
-                        { title: 'Work', text: 'Go home', completed: true, date: new Date(), _id: new ObjectId() },
-                    ]
-                }
-            ],
-            todos : [
-                { text: 'Pick up the package outside' },
-                { text: 'Drink Water' }
-            ]
+        });
 
-        })
+        // Update user1 to add the "General" category and todos
+        await User.findByIdAndUpdate(user1._id, {
+            $set: {
+                categories: [
+                    {
+                        name: 'General',
+                        goals: [
+                            { title: 'School report', text: 'Finish report', completed: false, date: new Date(), _id: new ObjectId() },
+                            { title: 'Attend Class', text: 'Attend Class', completed: true, date: new Date(), _id: new ObjectId() }
+                        ]
+                    }
+                ],
+                todos: [
+                    { text: 'Do Homework 1.2' }
+                ]
+            }
+        });
 
-        console.log('Database Seeded')
-        process.exit()
+        // Update user2 to add the "General" category and todos
+        await User.findByIdAndUpdate(user2._id, {
+            $set: {
+                categories: [
+                    {
+                        name: 'General',
+                        goals: [
+                            { title: 'test title', text: 'test', completed: false, date: new Date(), _id: new ObjectId() },
+                        ]
+                    }
+                ],
+                todos: [
+                    { text: 'Pick up the package outside' },
+                    { text: 'Drink Water' }
+                ]
+            }
+        });
+
+        console.log('Database Seeded');
+        process.exit();
     } catch (error) {
-        console.error('Error Seeding Database', error)
-        process.exit()
+        console.error('Error Seeding Database', error);
+        process.exit();
     }
-})
-
+});
